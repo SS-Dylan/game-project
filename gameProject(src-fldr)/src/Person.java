f90dd60203b8cb1165ae7c43f27ec3c01ca0499f
twarren51 <71302764+twarren51@users.noreply.github.com>
2020-11-15 16:39:35 -0500

    Created Person class

---
+++ gameProject(src-fldr)/src/Person.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public abstract class person
+{
+    private String name;
+    private ArrayList<String> thingsToSay = new ArrayList();
+    private Helmet helmet;
+    private Plackart plackart;
+    private EquipmentManager inventory;
+    private int maxHealth;
+    private int currentHealth;
+
+    public person(String Name) {
+
+        name= Name;
+        maxHealth= 50;
+    }
+
+    public String saySomething()
+    {
+        int randomIndex = (int)(Math.random()*thingsToSay.size());
+        return thingsToSay.get(randomIndex);
+    }
+
+    public boolean equip(Equipment e){
+        return true;
+    }
+    public int attack(){
+
+    }
+
+    /*public String getName(){
+    return"";
+    }*/
+
+    public String toString(){
+
+        return "";
+    }
+
+    public ArrayList<String> getThingsToSay(){
+
+    }
+
+    public void setThingsToSay(ArrayList<String> list){
+
+    }
+}
+
