df153147e804e77d5bd60802edf125cbf5ba667c
Dylan Shaffer <13596307+WRX-Dylan@users.noreply.github.com>
2020-12-06 00:18:51 -0500

    Update LevelManager.java

--- gameProject(src-fldr)/src/LevelManager.java
+++ gameProject(src-fldr)/src/LevelManager.java
@@ -1,51 +1,40 @@
+/**
+ * Dylan Shaffer
+ * CSCI 1260-901
+ */
+
 public class LevelManager Player {
 
-public void levelUp()
-{
-    if (currentExp == 100) {
-        playerLevel++;
-    } else if (currentExp == < 100)
-    {
-        playerLevel = getPlayerLevel;
+    public void levelUp() {
+        if (getExp == 100) {
+            playerLevel++;
+        } else if (currentExp == < 100) {
+            playerLevel = getPlayerLevel;
+        }
     }
-}
 
-public void earnExp()
-{
+    public void earnExp() {
 
+    }
 
-}
+    public int getExp() {
+        return Player.getCurrentExp();
+    }
 
-public int getExp()
-{
-    return getCurrentExp;
+    public void increaseWeaponDamage(int level) {
 
-}
+    }
 
-public void increaseWeaponDamage(int level)
-{
+    public void increaseFireDamage(int level) {
 
+    }
 
-}
+    public void increaseIceDamage(int level) {
 
-public void increaseFireDamage(int level)
-{
+    }
 
+    public String checkLevelUp() {
 
-}
-
-public void increaseIceDamage(int level)
-{
-
-
-}
-
-public String checkLevelUp()
-{
-
-    return playerLevel;
-}
-
-
-
+        return Player.getPlayerLevel();
+    }
 }
