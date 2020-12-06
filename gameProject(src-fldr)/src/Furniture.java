a0f39f37fad913252f9f33566599d9721cfa8a9d
Seth Gilstrap <gilstraps@etsu.edu>
2020-12-05 22:41:37 -0500

    Updates to Room and Furniture

--- gameProject(src-fldr)/src/Furniture.java
+++ gameProject(src-fldr)/src/Furniture.java
@@ -1,9 +1,44 @@
-public class Furniture {
+public abstract class Furniture implements IHitable{
 
     private String name;
     private int value;
     private String location;
     private int maxHealth;
     private int currentHealth;
-    
+
+
+    public Furniture()
+    {
+
+    }
+
+    public Furniture(String Name, String location, int value)
+    {
+        name = Name;
+        maxHealth = IHitable.maxHealth;
+        currentHealth = IHitable.currentHealth;     //SHOULD we make health variables specifically for furniture???
+        this.location = location;
+        this.value = value;
+    }
+
+    public String getLocation()
+    {
+        return location;
+    }
+
+    public int takeDamage(int dmg, int fire, int ice)
+    {
+        int totalDamage = dmg + ice + fire;
+        return totalDamage;
+    }
+
+    public String sellFurniture(Furniture target)
+    {
+
+    }
+
+    public String furnitureAttack(Furniture weapon, Person target)
+    {
+
+    }
 }
--- gameProject(src-fldr)/src/Room.java
+++ gameProject(src-fldr)/src/Room.java
@@ -1,7 +1,56 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Room {
 
     private String description;
     private String size;
     private int lightLevel;
+    private Arr
+
+    public Room()
+    {
+
+    }
+
+    public String getRoomSize()
+    {
+        return size;
+    }
+
+    public String lightLevel()
+    {
+
+    }
+
+    public int getLightLevel()
+    {
+        return lightLevel;
+    }
+
+    public String trap()
+    {
+
+    }
+
+    public String getPeople()
+    {
+
+    }
+
+    public String getFurniture()
+    {
+
+    }
+
+    public String getTraps()
+    {
+        return trap();
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
 
 }
