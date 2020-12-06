bb2023122aaa920989c538aa7a61daacd58b4a91
Dylan Shaffer <13596307+WRX-Dylan@users.noreply.github.com>
2020-12-05 22:57:14 -0500

    Update GameMap.java

--- gameProject(src-fldr)/src/GameMap.java
+++ gameProject(src-fldr)/src/GameMap.java
@@ -9,22 +9,25 @@ public class GameMap {
         {" A1 ", " B1 ", " C1 "},
         {" A2 ", " B2 ", " C2 "},
         {" A3 ", " B3 ", " C3 "}
-    };
+    }; //Game map layout in a simple 3x3 grid
 
-    public void showMap()
-    {
+    Room room = new Room(); //Initialize a room object for getRoomDescription
+
+    public void showMap() { //Print out the 3x3 grid
         System.out.println(rooms[0][0] + rooms[0][1] + rooms[0][2]);
         System.out.println(rooms[1][0] + rooms[1][1] + rooms[1][2]);
         System.out.println(rooms[2][0] + rooms[2][1] + rooms[2][2]);
     }
 
-    public String currentLocation()
-    {
-        return "";
+    public String currentLocation() {
+        return ""; //Find the current index and return it somehow
     }
 
-    public String getRoomDescription()
-    {
-        return "";
+    Public changeRoom() {  //Not fully sure what to do on this one
+
+    }
+
+    public String getRoomDescription() {
+        return Room.getDescription();
     }
 }
