e9faff818200e8bdc3b1b0625ddbab6988255510
Dylan Shaffer <13596307+WRX-Dylan@users.noreply.github.com>
2020-12-06 22:48:02 -0500

    Creation Files and Initializer
    Makes the files for the initalizer to use

--- gameProject(src-fldr)/src/GameMapInitializer.java
+++ gameProject(src-fldr)/src/GameMapInitializer.java
@@ -8,23 +8,28 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GameMapInitializer {
-    private static void roomGenerator(String fileEnd)
+    private static String[] generateRoom()
     {
-        String fileName = "Creation Files/Rooms";
+        String fileName = "CreationFiles/Rooms";
         Random rand = new Random();
         int i = 0;
         int randomLine = rand.nextInt(10);
         File readFile = new File(fileName);
         ArrayList<String> addedList = new ArrayList<String>();
         Scanner inputFile = new Scanner(readFile);
-        while(randomLine >= i)//reads the file line by line
+        while(randomLine >= i)//Reads the file line by line
         {
                 String currentLine = inputFile.nextLine();
-                String[] segments = currentLine.split("/|");
-                //code for getting values from specific lines
+                String[] segments = currentLine.split("|");
+                //Code for getting values from specific lines
                 i++;
         }
         i = 0;
+
+       //all the bullshit we gotta take from the textfile as it's own string to be passed to the parameterized room constructor 
+
+
         inputFile.close();
+        return "";
     }
 }
