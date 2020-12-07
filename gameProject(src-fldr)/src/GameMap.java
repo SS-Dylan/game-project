163ba3a5fc12e7b6507e2f5a73888eaf83abb683
Dylan Shaffer <13596307+WRX-Dylan@users.noreply.github.com>
2020-12-06 22:24:25 -0500

    Update GameMap.java

--- gameProject(src-fldr)/src/GameMap.java
+++ gameProject(src-fldr)/src/GameMap.java
@@ -5,9 +5,11 @@
 
 public class GameMap {
     private String[][] rooms = {
-        {" A1 ", " B1 ", " C1 "},
-        {" A2 ", " B2 ", " C2 "},
-        {" A3 ", " B3 ", " C3 "}
+        {" A1 ", " B1 ", " C1 ", " D1 ", " E1 "},
+        {" A2 ", " B2 ", " C2 ", " D2 ", " E2 "},
+        {" A3 ", " B3 ", " C3 ", " D3 ", " E3 "},
+        {" A4 ", " B4 ", " C4 ", " D4 ", " E4 "},
+        {" A5 ", " B5 ", " C5 ", " D5 ", " E5 "}
     }; //Game map layout in a simple 3x3 grid
 
     Room room = new Room(); //Initialize a room object for getRoomDescription
